88246e69faca6609bdac4f91e67f7659db381f6b
imran123509 <hasanimran114@gmail.com>
2023-12-28 13:40:23 +0530

    added bodyparser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const express=require('express');
 
+const bodyparser=require('body-parser')
+
 const {PORT}=require('./config/serverconfig');
 const app=express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}))
 
 app.listen(PORT, ()=>{
     console.log('server is connected');
